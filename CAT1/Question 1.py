652d5e54a71248b0c36147e109791a46845d8aad
GMwanda <George.Mwanda@strathmore.edu>
2023-09-26 14:28:14 +0300

    Creating functions for the processes.

--- CAT1/Question 1.py
+++ CAT1/Question 1.py
@@ -2,42 +2,53 @@ import json
 import pandas as pd
 import os
 
-# Directory containing JSONL files replace this with the path to where the files are located in your respective laptops
-jsonl_directory = '/School Stuuf/3.2 Notes/ComputerGraphics/ClassProjectCodes/CAT1/data/'
+def extract_data_from_jsonl(jsonl_directory):
+    language_data = {}
 
-# Dictionary to store data for each language
-language_data = {}
+    for filename in os.listdir(jsonl_directory):
+        if filename.endswith(".jsonl"):
+            file_path = os.path.join(jsonl_directory, filename)
 
-# Iterate over the JSONL files
-for filename in os.listdir(jsonl_directory):
-    if filename.endswith(".jsonl"):
-        file_path = os.path.join(jsonl_directory, filename)
+            # Extract language code from the filename, assuming the format is en-xx.jsonl
+            language_code = filename.split('.')[0]
 
-        # Extract language code from the filename, assuming the format is en-xx.jsonl
-        language_code = filename.split('.')[0]
+            # Initialize language-specific data dictionary
+            if language_code not in language_data:
+                language_data[language_code] = []
 
-        # Initialize language-specific data dictionary
-        if language_code not in language_data:
-            language_data[language_code] = []
+            with open(file_path, 'r', encoding='utf-8') as file:
+                for line in file:
+                    try:
+                        # Load JSON data from each line
+                        data = json.loads(line)
+                        language_data[language_code].append(data)
+                    except json.JSONDecodeError:
+                        # Handle invalid JSON lines (skip or log as needed)
+                        print(f"Skipping invalid JSON line in {filename}: {line}")
 
-        with open(file_path, 'r', encoding='utf-8') as file:
-            for line in file:
-                try:
-                    # Load JSON data from each line
-                    data = json.loads(line)
-                    language_data[language_code].append(data)
-                except json.JSONDecodeError:
-                    # Handle invalid JSON lines (skip or log as needed)
-                    print(f"Skipping invalid JSON line in {filename}: {line}")
+    return language_data
 
-# Iterate over language data and create DataFrames
-for language_code, data in language_data.items():
-    # Create a DataFrame for the language
-    df = pd.DataFrame(data, columns=['id', 'utt', 'annot_utt'])
+def create_and_export_excel_files(language_data, output_directory):
+    for language_code, data in language_data.items():
+        # Create a DataFrame for the language with the specified columns
+        df = pd.DataFrame(data, columns=['id', 'utt', 'annot_utt'])
 
-    # Define Excel filename using language code
-    excel_filename = f'{language_code}.xlsx'
+        # Define Excel filename using language code
+        excel_filename = f'{language_code}.xlsx'
 
-    # Export DataFrame to Excel
-    df.to_excel(excel_filename, index=False)
+        # Export DataFrame to Excel
+        excel_path = os.path.join(output_directory, excel_filename)
+        df.to_excel(excel_path, index=False)
 
+if __name__ == "__main__":
+    # Path containing the JSONL files. (Replace this with the path in your respective laptops.)
+    jsonl_directory = '/School Stuuf/3.2 Notes/ComputerGraphics/ClassProjectCodes/CAT1/data/'
+
+    # Output directory for Excel files
+    excel_output_directory = '/School Stuuf/3.2 Notes/ComputerGraphics/ClassProjectCodes/CAT1/Excel Files Q1/'
+
+    # Extract data from JSONL files
+    language_data = extract_data_from_jsonl(jsonl_directory)
+
+    # Create and export Excel files
+    create_and_export_excel_files(language_data, excel_output_directory)
