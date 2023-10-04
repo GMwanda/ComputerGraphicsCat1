05087aba9ff23ab79b1b25222528401ede7bfb34
GMwanda <George.Mwanda@strathmore.edu>
2023-10-04 15:48:01 +0300

    Split the code for questions 2 and 3 into functions

--- CAT1/question3.py
+++ CAT1/question3.py
@@ -1,44 +1,43 @@
 import json
 import os
 
+
+def read_jsonl_file(file_path):
+    with open(file_path, 'r') as jsonl_file:
+        return [json.loads(line) for line in jsonl_file]
+
+
+def write_to_output_file(data_list, output_path):
+    with open(output_path, 'a') as output_file:
+        for record in data_list:
+            json.dump(record, output_file)
+            output_file.write('\n')
+
+
+def process_language_files(input_file_paths, output_file_path):
+    data_lists = [read_jsonl_file(file_path) for file_path in input_file_paths]
+
+    translations = []
+    for entries in zip(*data_lists):
+        translation_entry = {
+            "id": entries[0].get("id"),
+            "en": entries[0].get("utt"),
+            "ger": entries[1].get("utt"),
+            "swa": entries[2].get("utt"),
+        }
+        translations.append(translation_entry)
+
+    translations_json = {"translations": translations}
+    write_to_output_file([translations_json], output_file_path)
+
+
 current_directory = os.path.dirname(os.path.abspath(__file__))
-english_train_file_path = os.path.join(current_directory, 'eng-train.jsonl')
-german_train_file_path = os.path.join(current_directory, 'ger-train.jsonl')
-swahili_train_file_path = os.path.join(current_directory, 'swa-train.jsonl')
+input_file_paths = [
+    os.path.join(current_directory, 'eng-train.jsonl'),
+    os.path.join(current_directory, 'ger-train.jsonl'),
+    os.path.join(current_directory, 'swa-train.jsonl')
+]
 output_file_path = os.path.join(current_directory, 'translations.json')
-english_train_data = []
-german_train_data = []
-swahili_train_data = []
 
-with open(english_train_file_path, 'r') as eng_train_file:
-    for line in eng_train_file:
-        data = json.loads(line)
-        english_train_data.append(data)
-
-with open(german_train_file_path, 'r') as ger_train_file:
-    for line in ger_train_file:
-        data = json.loads(line)
-        german_train_data.append(data)
-
-with open(swahili_train_file_path, 'r') as swa_train_file:
-    for line in swa_train_file:
-        data = json.loads(line)
-        swahili_train_data.append(data)
-
-translations = []
-for eng_data, ger_data, swa_data in zip(english_train_data, german_train_data, swahili_train_data):
-    eng_utt = eng_data.get("utt")
-    ger_utt = ger_data.get("utt")
-    swa_utt = swa_data.get("utt")
-    translation_entry = {
-        "id": eng_data.get("id"),
-        "en": eng_utt,
-        "ger": ger_utt,
-        "swa": swa_utt,
-    }
-    translations.append(translation_entry)
-
-translations_json = {"translations": translations}
-with open(output_file_path, "w", encoding="utf-8") as json_file:
-    json.dump(translations_json, json_file, ensure_ascii=False, indent=4)
+process_language_files(input_file_paths, output_file_path)
 print(f"Translations saved to '{output_file_path}'")
